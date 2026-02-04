706a76467a91e68c7e18de78ec4eda5b0118037f
Ignacio <ignaciobrenas@gmail.com>
2026-02-04 12:07:03 +0100

    STAR STUCOM (clases organizadas Vista ModelO Control)

---
+++ src/main/java/controller/CensusControler.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package controller;
+
+/**
+ *
+ * @author ignac
+ */
+public class CensusControler {
+    
+}
--- src/main/java/view/Inicio.java
+++ src/main/java/view/Inicio.java
@@ -1,13 +1,14 @@
+package view;
+
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-
-package main;
 
 /**
  *
  * @author ignac
  */
-public class CensusControler {
+public class Inicio {
     
 }
