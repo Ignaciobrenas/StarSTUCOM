ee59e6675c8bffddb9a15ac38fd6c290339cd55c
Ignacio <ignaciobrenas@gmail.com>
2026-02-10 13:50:55 +0100

    STAR STUCOM (clase vulcaniano creada con condiciones de indice de meditacion)

--- src/main/java/model/Vulcaniano.java
+++ src/main/java/model/Vulcaniano.java
@@ -8,6 +8,45 @@ package model;
  *
  * @author ignac
  */
-public class Vulcaniano {
+public class Vulcaniano extends Ser{
+    private static int nivelCivilizacion = 3 ;
+    private static int indiceMeditacion;
+
+    public Vulcaniano(String nombre) {
+        super(nombre);
+    }
+
+    public static int getNivelCivilizacion() {
+        return nivelCivilizacion;
+    }
+
+    public static void setNivelCivilizacion(int nivelCivilizacion) {
+        Vulcaniano.nivelCivilizacion = nivelCivilizacion;
+    }
+
+    public static int getIndiceMeditacion() {
+        return indiceMeditacion;
+    }
+
+    public static void setIndiceMeditacion(int indiceMeditacion) {
+        if (indiceMeditacion >= 0 && indiceMeditacion <= 10) {
+            Vulcaniano.indiceMeditacion = indiceMeditacion;
+        } else {
+            System.out.println("Error: El indice de mediacion debe estar entre 0 y 130");
+        };
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    @Override
+    public String toString() {
+        return "Vulcaniano{" + '}';
+    }
     
 }
