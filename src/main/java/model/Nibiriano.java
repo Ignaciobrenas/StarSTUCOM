6e63a738cc864d1ef9e233c993979e56f1b7029b
Ignacio <ignaciobrenas@gmail.com>
2026-02-10 14:08:44 +0100

    STAR STUCOM (clase nibiriano)

--- src/main/java/model/Nibiriano.java
+++ src/main/java/model/Nibiriano.java
@@ -8,6 +8,41 @@ package model;
  *
  * @author ignac
  */
-public class Nibiriano {
+public class Nibiriano extends Ser{
+    private static int nivelCivilizacion = 2;
+    private static boolean vegetariano;
     
+
+    public Nibiriano(String nombre) {
+        super(nombre);
+    }
+
+    public static int getNivelCivilizacion() {
+        return nivelCivilizacion;
+    }
+
+    public static void setNivelCivilizacion(int nivelCivilizacion) {
+        Nibiriano.nivelCivilizacion = nivelCivilizacion;
+    }
+
+    public static boolean isVegetariano() {
+        return vegetariano;
+    }
+
+    public static void setVegetariano(boolean vegetariano) {
+        Nibiriano.vegetariano = vegetariano;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    @Override
+    public String toString() {
+        return "Nibiriano{" + '}';
+    }
 }
