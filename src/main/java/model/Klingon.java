5349675e4d355399413283492cb7843845e28bd7
Ignacio <ignaciobrenas@gmail.com>
2026-02-11 11:53:04 +0100

    STAR STUCOM (clase Klingon)

--- src/main/java/model/Klingon.java
+++ src/main/java/model/Klingon.java
@@ -8,6 +8,41 @@ package model;
  *
  * @author ignac
  */
-public class Klingon {
-    
+public class Klingon extends Ser {
+
+    private static int nivelCivilizacion = 3;
+    private static int nivelFuerza;
+
+    public Klingon(String nombre) {
+        super(nombre);
+    }
+
+    public static int getNivelCivilizacion() {
+        return nivelCivilizacion;
+    }
+
+    public static void setNivelCivilizacion(int nivelCivilizacion) {
+        Klingon.nivelCivilizacion = nivelCivilizacion;
+    }
+
+    public static int getNivelFuerza() {
+        return nivelFuerza;
+    }
+
+    public void setNivelFuerza(int nivelFuerza) {
+        if (nivelFuerza >= 50 && nivelFuerza <= 350) {
+            this.nivelFuerza = nivelFuerza;
+        } else {
+            System.out.println("Error: el nivel de fuerza debe estar entre 50 y 350");
+        }
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
 }
