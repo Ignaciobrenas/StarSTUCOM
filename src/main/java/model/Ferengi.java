f57d2ad84db093232206e802ba534e0cb2e038d1
Ignacio <ignaciobrenas@gmail.com>
2026-02-11 11:59:45 +0100

    STAR STUCOM (clase Ferengi)

--- src/main/java/model/Ferengi.java
+++ src/main/java/model/Ferengi.java
@@ -9,6 +9,44 @@ package model;
  *
  * @author ignac
  */
-public class Ferengi {
+public class Ferengi extends Ser {
+    private static int nivelCivilizacion = 1;
+    private static boolean latinum;
+
+    public Ferengi(String nombre) {
+        super(nombre);
+    }
+
+    public static int getNivelCivilizacion() {
+        return nivelCivilizacion;
+    }
+
+    public static void setNivelCivilizacion(int nivelCivilizacion) {
+        Ferengi.nivelCivilizacion = nivelCivilizacion;
+    }
+
+    public static boolean isLatinum() {
+        return latinum;
+    }
+
+    public static void setLatinum(boolean latinum) {
+        Ferengi.latinum = latinum;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+     @Override
+    public String toString() {
+        return super.toString() + ", Ferengi{" +
+                "nivelCivilizacion=" + nivelCivilizacion +
+                ", latinum=" + latinum +
+                '}';
+    }
     
 }
