56ae0701f3da7f4a80535a5bc33a4ec880ae9f51
Ignacio <ignaciobrenas@gmail.com>
2026-02-11 12:19:22 +0100

    STAR STUCOM (Base de los metodos)

--- src/main/java/controller/CensusController.java
+++ src/main/java/controller/CensusController.java
@@ -11,15 +11,19 @@ package controller;
 public class CensusController {
 
     public static void registrarPlaneta() {
+        System.out.println("--------------------Registar un planeta--------------------");
     }
 
     public static void censarSer() {
+        System.out.println("--------------------Censar un ser en un planeta--------------------");
     }
 
     public static void borrarSer() {
+        System.out.println("--------------------Borrar un ser--------------------");
     }
 
     public static void obtenerSerPorNombre() {
+        System.out.println("--------------------Obtener un ser por su nombre--------------------");
     }
     
 }
